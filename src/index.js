86826775734b6325849dc5dd3ca3a4dec3c093ba
edyluro <edgarla.isc@gmail.com>
2020-05-17 17:35:32 +0200

    Revert "Develop countdown"

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
-import CountdownGame from "./CountdownGame";
 
 const theme = {
-	primary: "#136f97",
-	secondary: "#22bcff",
-	primaryPiece: "#009680",
-	secondaryPiece: "#1d305b",
-	primaryFrame: "#343a40",
-	successFrame: "#7bc44c",
-	dangerFrame: "#c52d30",
+	primary: "#1f779d",
+	secondary: "#009680",
 };
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<CountdownGame />
-		</ThemeProvider>
+		<ThemeProvider theme={theme} />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
